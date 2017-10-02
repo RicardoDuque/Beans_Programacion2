ed654e87136f9927b44483272dcf62c744537452
RicardoDuque <rdg1421@gmail.com>
2017-10-01 23:10:15 -0500

    Separación lógica tienda
    Separo lógica de la tienda y los puntos.
    Falta para entrega:
    -Al comprar un character, cambiar estado a "comprado" y no descontar
    dinero

--- Juego_Final/Assets/Scripts/CharacterSelector.cs
+++ Juego_Final/Assets/Scripts/CharacterSelector.cs
@@ -10,10 +10,15 @@ public class CharacterSelector : MonoBehaviour {
     public static GameObject Characters1, Characters2, Characters3;
     public GameObject[] characters = new GameObject[3] { Characters1, Characters2, Characters3 }; //Se puede hacer mejor con GetChilds
     private int i = 0;
+    public Points PointsScript;
+    public int characterPrice = 5;
+
 
     // Use this for initialization
     void Start()
     {
+        //Points points = GetComponent<Points>();//Acceder a la variable points del script Points
+
         i = PlayerPrefs.GetInt("CharacterSelected");
 
         foreach(GameObject go in characters)
@@ -52,9 +57,28 @@ public class CharacterSelector : MonoBehaviour {
         
     }
 
+    public void ConfirmSelection()
+    {
+        if (Points.points >= characterPrice)
+        {
+            CharacterSelection();
+        }
+
+        LoadLevel();
+
+    }
+
+    public void LoadLevel()
+    {
+        SceneManager.LoadScene("EscenaPrueba");
+    }
+
     public void CharacterSelection()
     {
+        Points.points -= 5;//Resta 5 al puntaje global
+        PointsScript.UpdatePoints(Points.points);
+
+        //Points.points =- 1;Resto puntos
         PlayerPrefs.SetInt("CharacterSelected", i);
-        SceneManager.LoadScene("EscenaPrueba");
     }
 }
